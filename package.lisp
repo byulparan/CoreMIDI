229550a83ee802f16ed4d0813bb3e479dd71b616
Didier Verna <didier@didierverna.net>
2017-06-09 18:46:24 +0200

    Update the exported symbols.

--- package.lisp
+++ package.lisp
@@ -1,25 +1,52 @@
-
 (defpackage #:midi
   (:use #:cl)
-  (:export #:initialize
-	   #:midi-restart
-	   #:midiobject-display-name
-	   #:get-source
-	   #:get-destination
-	   #:find-source
-	   #:find-destination
-	   #:list-of-sources
-	   #:list-of-destinations
-	   #:set-midi-callback
-	   #:midi-send
-	   #:midi-send-at
+  (:export
+    ;; From cffi.lisp:
+    ;; Data Types
+    #:object-ref
+    #:device-ref
+    #:entity-ref
+    #:endpoint-ref
+    ;; MIDI Devices:
+    #:device-get-entity
+    #:device-get-number-of-entities
+    #:get-device
+    #:get-number-of-devices
+    ;; MIDI Entities
+    #:entity-get-destination
+    #:entity-get-device
+    #:entity-get-number-of-destinations
+    #:entity-get-number-of-sources
+    #:entity-get-source
+    ;; MIDI Endpoints:
+    #:destination-create
+    #:endpoint-dispose
+    #:endpoint-get-entity
+    #:get-destination
+    #:get-number-of-destinations
+    #:get-number-of-sources
+    #:get-source
+    #:source-create
 
-	   #:add-midi-notify-callback
-	   #:set-midi-notify-callback
-	   #:+setup-changed+
-	   #:+object-added+
-	   #:+object-removed+
-	   #:+property-changed+
-	   #:+thru-connections-changed+
-	   #:+serial-port-owner-changed+
-	   #:+io-error+))
+    #:initialize
+    #:midi-restart
+    #:midiobject-display-name
+    #:get-source
+    #:get-destination
+    #:find-source
+    #:find-destination
+    #:list-of-sources
+    #:list-of-destinations
+    #:set-midi-callback
+    #:midi-send
+    #:midi-send-at
+
+    #:add-midi-notify-callback
+    #:set-midi-notify-callback
+    #:+setup-changed+
+    #:+object-added+
+    #:+object-removed+
+    #:+property-changed+
+    #:+thru-connections-changed+
+    #:+serial-port-owner-changed+
+    #:+io-error+))
